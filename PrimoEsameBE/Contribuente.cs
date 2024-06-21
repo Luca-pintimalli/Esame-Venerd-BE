99d731d6623d3d9c7762a1e37ffeb466f7f88b76
lucapintimalli <luca.pintimalli99@gmail.com>
2024-06-21 14:03:02 +0200

    Prova settimanale-Modulo1-BE

---
+++ PrimoEsameBE/Contribuente.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+namespace PrimoEsameBE
+{
+	public class Contribuente
+	{
+
+        //Proprietà
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string  CodiceFiscale { get; set; }
+        public string Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public int RedditoAnnuale { get; set; }
+
+
+
+
+        //ho scelto di usare un solo costruttore visto l'esercizio.
+        public Contribuente(string nome, string cognome,DateTime dataNascita,string codiceFiscale, string sesso,string comuneResidenza , int redditoAnnuale )
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+
+        }
+
+        //metodo calcolo imposta, utilizzo decimal essendo che andiamo a clcolare numeri decimali 
+        public decimal CalcoloImposta()
+        {
+            //imposto l'imposta iniziale a 0
+
+            decimal imposta = 0;
+
+            //calcolo in base al reddito l'imposta
+
+            if(RedditoAnnuale <= 15000)
+            {
+                imposta = RedditoAnnuale * 0.23m; //essendo double inserisco la 'm' alla cifra
+            }
+
+            else if(RedditoAnnuale > 15000 && RedditoAnnuale <= 28000)
+            {
+                imposta = 3450 + (RedditoAnnuale - 15000) * 0.27m;
+            }
+
+            else if (RedditoAnnuale > 28000 && RedditoAnnuale <= 55000)
+            {
+                imposta = 6960 + (RedditoAnnuale - 28000) * 0.38m;
+            }
+
+            else if (RedditoAnnuale > 55000 && RedditoAnnuale <= 75000)
+            {
+                imposta = 17220 + (RedditoAnnuale - 55000) * 0.41m;
+            }
+            else if(RedditoAnnuale > 75000)
+            {
+                imposta = 25420 + (RedditoAnnuale - 75000) * 0.43m; 
+            }
+
+            return imposta; 
+        }
+		
+	}
+}
+
---
+++ PrimoEsameBE/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimoEsameBE;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //CREO UN OGGETTO CONTRIBUENTE 
+        Contribuente contribuente = new Contribuente("Pino", "Pano", new DateTime(1980, 9, 12), "PNIPNA80T09L219G", "M", "TORINO", 1000000);
+
+   
+        //CALCOLO DELL'IMPOSTA
+        decimal impostaDaPagare = contribuente.CalcoloImposta();
+
+
+
+        Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome}");
+        Console.WriteLine($"nato il :{contribuente.DataNascita.ToShortDateString()}({contribuente.Sesso})");
+        Console.WriteLine($"Residente in: {contribuente.ComuneResidenza}");
+        Console.WriteLine($"CF: {contribuente.CodiceFiscale}");
+        Console.WriteLine($"Reddito dichiarato: {contribuente.RedditoAnnuale}€"); Console.WriteLine($"L'imposta da pagare è: {impostaDaPagare}€");
+    }
+}
+
